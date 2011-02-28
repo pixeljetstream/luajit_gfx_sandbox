db209f30557c599484e739b295e9ab2823d4199a
crazybutcher <crazybutcher@luxinia.de>
2011-02-28 10:34:15 +0100

    ffihelper: changed to pure size-based newuserdata

--- plugins/ffihelper/ffihelper.c
+++ plugins/ffihelper/ffihelper.c
@@ -5,53 +5,40 @@
 
 #include <string.h>
 
-static int touserdata(lua_State *L)
+static int newuserdata(lua_State *L)
 {
   int stacksize = lua_gettop(L);
-  int size;
-  const void* orig;
-  double num;
+  size_t size;
 
-  if (stacksize < 3){
-    lua_pushstring(L,"1 number, 1 number, 1 table required");
+  if (stacksize < 2){
+    lua_pushstring(L,"1 number, 1 table required");
     lua_error(L);
     return 1;
   }
 
-  num = lua_tonumber(L,-3);
-  size = (int)luaL_checknumber(L,-2);
-  orig = (const void*)(uintptr_t)num;
+  size = (size_t)luaL_checkinteger(L,-2);
 
-  if (!orig || !lua_istable(L,-1)){
-    lua_pushstring(L,"1 number, 1 number, 1 table required");
+  if (!size || !lua_istable(L,-1)){
+    lua_pushstring(L,"1 number, 1 table required");
     lua_error(L);
     return 1;
   }
-  // stack: light, number, table
+  // stack: number, table
   
-  if (!size){
-    // store pointer value
-    const void** udata = lua_newuserdata(L,sizeof(void*));
-    udata[0] = orig;
-  }
-  else{
-    // deepcopy
-    void* udata = lua_newuserdata(L,size);
-    memcpy(udata,orig,size);
-  }
-  // stack: light, number, table, udata
+  lua_newuserdata(L,size);
+  // stack: number, table, udata
 
   lua_pushvalue(L,-2);
-  // stack: light, number, table, udata, table
+  // stack: number, table, udata, table
 
   lua_setmetatable(L,-2);
-  // stack: light, number, table, udata
+  // stack: number, table, udata
 
   return 1;
 }
 
 static const struct luaL_reg ffihelperLib[] = {
-  {"touserdata", touserdata},
+  {"newuserdata", newuserdata},
   {NULL, NULL},
 };
 
