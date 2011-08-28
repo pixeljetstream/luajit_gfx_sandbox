fd85d919463a43d1628b0a2e33fead0641570125
crazybutcher <crazybutcher@luxinia.de>
2011-08-28 14:30:18 +0200

    updated glfwminimal sample to include some gl

--- samples/glfwminimal/main.lua
+++ samples/glfwminimal/main.lua
@@ -1,25 +1,32 @@
-print "hello"
+print "glfw-minimal-host"
 
 local glfw = require "glfw"
-local ffi = require "ffi"
+local ffi  = require "ffi"
+local gl   = require "glewgl"
+local svPoll = LXG.svPoll
 
 local function main()
-   assert( glfw.glfwInit() )
-   local window = glfw.glfwOpenWindow( 640, 480, glfw.GLFW_WINDOWED, "Spinning Triangle", nil)
-   assert( window )
-   glfw.glfwEnable(window, glfw.GLFW_STICKY_KEYS);
-   glfw.glfwSwapInterval(1);
-   while glfw.glfwIsWindow(window) and glfw.glfwGetKey(window, glfw.GLFW_KEY_ESCAPE) ~= glfw.GLFW_PRESS 
-   do
+  assert( glfw.glfwInit() )
+  local window = glfw.glfwOpenWindow( 640, 480, glfw.GLFW_WINDOWED, "glfw-minimal-host", nil)
+  assert( window )
+  gl.glewInit()
+  gl.glClearColor(0.2,0.2,0.2,0)
+  local clearbits = bit.bor(bit.bor(gl.GL_COLOR_BUFFER_BIT,gl.GL_STENCIL_BUFFER_BIT),gl.GL_DEPTH_BUFFER_BIT)
+
+  glfw.glfwEnable(window, glfw.GLFW_STICKY_KEYS);
+  glfw.glfwSwapInterval(1);
+  while glfw.glfwIsWindow(window) ~= 0 and glfw.glfwGetKey(window, glfw.GLFW_KEY_ESCAPE) ~= glfw.GLFW_PRESS 
+  do
     local t = glfw.glfwGetTime()
     if( THINK ) then THINK(t) end
-    
+    gl.glClear(clearbits)
+
     glfw.glfwSwapBuffers()
     glfw.glfwPollEvents()
-    LXG.svPoll()
-   end
-   glfw.glfwTerminate()
-   os.exit(0)
+    svPoll()
+  end
+  glfw.glfwTerminate()
+  os.exit(0)
 end
 
 main()
